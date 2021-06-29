dacad65ab76bccc6808937637bb6257f0c4741aa
AJ Pilkerton <apilkerton3618@gmail.com>
2021-06-29 14:23:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] games = {"Chess", "Scrable", "Poker", "Football",
+                              "Soccer", "Halo", "Monopoly", "Golf"};
+
+            var gameList = new List<string>(games);
+
+
+            gameList.ForEach(Console.WriteLine);
+
+            Console.WriteLine("#################################");
+
+            var sortedGames = new List<string>();
+            sortedGames = gameList.OrderBy(x => x.Length).ToList();
+            sortedGames.ForEach(Console.WriteLine);
+        }
+    }
+}
